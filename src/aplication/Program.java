5d53945a5e7e6714caf313858a629c1bf8de7232
paulohbraga <paulo@someletras.com.br>
2019-08-12 11:52:45 -0300

    Implemented interface Comparable, new list.txt with names and salaries

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,5 +1,34 @@
 package aplication;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Employee;
+
 public class Program {
+	
+	public static void main(String[] args) {
+		
+		List<Employee> employees = new ArrayList<>();
+		String path = "src/file.txt";
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+			String Employee = reader.readLine();
+			while(Employee != null) {
+				String[] fields = Employee.split(",");
+				employees.add(new Employee(fields[0], fields[1]));
+				Employee = reader.readLine();
+			}
+		}catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		Collections.sort(employees);
+		for(Employee s: employees) {
+			System.out.println(s.getName() + ", " + s.getSalary());
+		}
+	}
 
 }
--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,11 +1,42 @@
 package entities;
 
+
 public class Employee implements Comparable<Employee> {
 
+	private String name;
+	private String salary;
+	
+	
+	public Employee(String name, String salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+
 	@Override
-	public int compareTo(Employee o) {
+	public int compareTo(Employee next) {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.name.compareTo(next.getName());
 	}
 
 }
