ee59eb947d2f31b854269a7da0a9cffafd32dec2
paulohbraga <paulo@someletras.com.br>
2019-08-12 00:25:19 -0300

    Partil commit

---
+++ src/entities/Employee.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Employee implements Comparable<Employee> {
+
+	@Override
+	public int compareTo(Employee o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
